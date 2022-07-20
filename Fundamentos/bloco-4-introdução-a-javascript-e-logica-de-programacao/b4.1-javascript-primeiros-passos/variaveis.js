0e000a2795be153062d27eb376853d86bc953f1c
Vinicius Santiago <vinicius.santiago997@gmail.com>
2022-07-20 15:12:59 -0300

    Exercicio para fixar sobre Variaveis

---
+++ Fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/b4.1-javascript-primeiros-passos/variaveis.js
@@ -0,0 +1,17 @@
+const myName = "Vinicius"
+const birthCity = "Jundiai"
+let birthYear = 1997
+
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
+
+birthYear = 2030
+
+console.log(birthYear)
+
+birthCity = "São Paulo"
+
+console.log(birthCity)
+
+/* Não foi possível alterar o birthCity, pois é uma constante (foi declarada com "const")*/
