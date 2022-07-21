3cfb9afb865264847c3292bfa4707406a9d402d7
Vinicius Santiago <vinicius.santiago997@gmail.com>
2022-07-21 15:03:25 -0300

    Exercicio 5 do dia 4.1

--- Fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/b4.1-javascript-primeiros-passos/main.js
+++ Fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/b4.1-javascript-primeiros-passos/main.js
@@ -1,23 +1,11 @@
 /* Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para: */
 
 
-let a = 10
-let b = 12
-/* 
-// Adição
-let adicao = a + b
-console.log(adicao) */
+let a = 13
+let b = 3
 
 
-//Subtração
+// Modulo
 
-/* let subtracao = a - b
-console.log(subtracao) */
-
-// multiplicacao
-/* let multiplicacao = a * b
-console.log(multiplicacao) */
-
-//Divisao
-let divisao = a / b
-console.log(divisao) 
+let modulo = a % b
+console.log(modulo)
