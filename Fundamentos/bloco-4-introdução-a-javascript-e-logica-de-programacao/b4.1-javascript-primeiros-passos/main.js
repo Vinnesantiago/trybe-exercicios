7efab168a1aa29108a8e280ef2175877d136e7f7
Vinicius Santiago <vinicius.santiago997@gmail.com>
2022-07-21 15:47:15 -0300

    Exercicio 4 do dia 4.1

--- Fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/b4.1-javascript-primeiros-passos/main.js
+++ Fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/b4.1-javascript-primeiros-passos/main.js
@@ -1,13 +1,12 @@
-/* Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados. */
+/* Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário. */
 
-let numero1 = 100
-let numero2 = 150
-let numero3 = 77
+let numero = 0
 
-if(numero1 > numero2 && numero1 > numero3){
-  console.log(`O numero ${numero1} é maior que os numeros ${numero2} e ${numero3}`)
-} else if (numero2 > numero1 && numero2 > numero3) {
-  console.log(`O numero ${numero2} é maior que os numeros ${numero1} e ${numero3}`)
+
+if(numero > 0){
+  console.log("positive")
+} else if (numero < 0){
+  console.log("negative")
 } else {
-  console.log(`O numero ${numero3} é maior que os numeros ${numero1} e ${numero2}`)
+  console.log("zero")
 }
