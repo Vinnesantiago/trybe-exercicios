c5760c80fc518489cf92ba48ea22d400a35c635a
Vinicius Santiago <vinicius.santiago997@gmail.com>
2022-07-22 16:16:11 -0300

    Exercicio 9 realizado

--- Fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/b4.2-javascript-array-e-loop-for/main.js
+++ Fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/b4.2-javascript-array-e-loop-for/main.js
@@ -96,5 +96,11 @@ for( let index = 1; index <=25;index++){
   array.push(index)
 }
 
-console.log(array)
+/* console.log(array) */
+
+/* Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2. */
+
+for(let index = 0; index < array.length;index++){
+ console.log(array[index] / 2)
+}
 
