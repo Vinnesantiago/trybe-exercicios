4bc7582fe5542c0381ff78b15507a5027a7fead5
Vinicius Santiago <vinicius.santiago997@gmail.com>
2022-07-25 19:12:32 -0300

    exercicio 3 4 e 5 realizado

--- Fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/b4.4-javascript-objetos-e-funcoes/main.js
+++ Fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/b4.4-javascript-objetos-e-funcoes/main.js
@@ -14,6 +14,36 @@ console.log("Bem vinda, ", info.personagem)
 
 info.recorrente = "Sim"
 
+
+console.log(info)
+
+console.log( "abaixo exercicio 3")
+
+/* 3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console: */
+
 for(let key in info){
   console.log(key)
+}
+
+/* 4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console: */
+
+for(let key in info){
+  console.log(info[key])
+}
+
+
+/* 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console: */
+
+let info2 = {
+  personagem: "Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O ultimo MacPatinhas",
+  recorrente: "Sim"
+}
+
+console.log(info.personagem, " e ", info2.personagem)
+console.log(info.origem, " e ", info2.origem)
+console.log(info.nota, " e ", info2.nota)
+if(info.recorrente === info2.recorrente){
+  console.log("Ambos recorrentes")
 }
