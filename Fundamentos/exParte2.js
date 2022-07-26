159d02bed3e4837e480da29c55661630dae7ff29
Vinicius Santiago <vinicius.santiago997@gmail.com>
2022-07-25 22:35:52 -0300

    exercicio 2 parte 2

--- Fundamentos/exParte2.js
+++ Fundamentos/exParte2.js
@@ -1,7 +1,7 @@
 
 /* 1 - Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for. */
 
- function isPalindrome(string){
+/*  function isPalindrome(string){
   // declarar a variavel stringForArray para transformar a string em um item de array
   let stringForArray = string.split("")
   // Criar a variavel para armazenar a stringforarray em reverse
@@ -16,7 +16,22 @@
   
 }
 console.log(isPalindrome("radar"))
+ */
 
+/* 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+Array de teste: [2, 3, 6, 7, 10, 1];.
+Valor esperado no retorno da função: 4. */
 
+function maxIndex(array){
+  let biggerItem = 0
+  for(let index =0; index < array.length;index++){
+    if(array[index] > biggerItem){
+      biggerItem = array[index]
+    }
+  }
+  let indexOfBiggerItem = array.indexOf(biggerItem)
+  return indexOfBiggerItem
 
+}
 
+console.log(maxIndex([2, 3, 6, 7, 10, 1,9,12,34,100,234,2,4,2,5,6]))
