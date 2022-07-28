a9a8b291eb048d3b02f2e165e50f81563a209758
Vinicius Santiago <vinicius.santiago997@gmail.com>
2022-07-28 18:06:15 -0300

    Exercicios do dia 5.1  Realizados

--- Fundamentos/bloco-5-javaScript-DOM-WebStorage/exercicio-5.1/main.js
+++ Fundamentos/bloco-5-javaScript-DOM-WebStorage/exercicio-5.1/main.js
@@ -14,7 +14,45 @@ Crie e execute uma função que exiba o conteúdo de todas as tags <p> no consol
 function changeText() {
   let changePText = document.getElementsByTagName("p")[1];
   changePText.innerHTML =
-    "Tabalhando bastante e me desenvolvendo cada vez mais";
+    "Trabalhando bastante e me desenvolvendo cada vez mais";
+}
+changeText();
+
+function changeBackground() {
+  let elementToChange = document.getElementsByClassName("main-content")[0];
+  elementToChange.style.background = "rgb(76,164,109)";
 }
 
-changeText();
+changeBackground();
+
+function changeBackgroundCenter() {
+  let elementToChange = document.getElementsByClassName("center-content")[0];
+  elementToChange.style.background = "white";
+}
+
+changeBackgroundCenter();
+
+function changeTitleText() {
+  let h1Text = document.getElementsByClassName("title")[0];
+  h1Text.innerHTML = "Exercício 5.1 - JavaScript";
+}
+
+changeTitleText();
+
+function transformToLowerCaseP() {
+  let p = document.getElementsByTagName("p");
+  for (let index = 0; index < p.length; index += 1) {
+    p[index].innerText = p[index].innerText.toUpperCase();
+  }
+}
+
+transformToLowerCaseP();
+
+function ShowEveryPInConsole() {
+  let p = document.getElementsByTagName("p");
+  for (let index = 0; index < p.length; index += 1) {
+    console.log(p[index].innerText);
+  }
+}
+
+ShowEveryPInConsole();
