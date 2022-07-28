fce6470388fb3ee146c916c43dd3d92e8dfda12a
Vinicius Santiago <vinicius.santiago997@gmail.com>
2022-07-28 13:18:08 -0300

    Exercicio do conteudo

---
+++ Fundamentos/bloco-5-javaScript-DOM-WebStorage/seletoresDeElementos.js
@@ -0,0 +1,16 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+const title = document.getElementById("page-title");
+
+title.innerText = "Assassin's Creed - Revelations";
+
+const secondParagraph = document.getElementById("second-paragraph");
+
+secondParagraph.style.backgroundColor = "red";
+secondParagraph.style.color = "white";
+secondParagraph.innerText = "Nada é verdade, tudo é permitido";
+
+let subtitle = document.getElementById("subtitle");
+
+subtitle.innerText = "Trabalhamos nas sombras para servir a luz.";
