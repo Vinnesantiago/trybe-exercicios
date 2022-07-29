472dce1a51a86607580f130b60e0dd583003d88a
Vinicius Santiago <vinicius.santiago997@gmail.com>
2022-07-29 15:17:57 -0300

    Exercicios do dia 5.2 realizados

--- Fundamentos/bloco-5-javaScript-DOM-WebStorage/exercicio-5.2/main.js
+++ Fundamentos/bloco-5-javaScript-DOM-WebStorage/exercicio-5.2/main.js
@@ -127,3 +127,35 @@ function addClassOnH3Element() {
 addClassOnH3Element();
 
 /* 🚀3 Remova a section criado no passo 5 (aquele que possui a classe left-content). Utilize a função .removeChild(); */
+function removeLeftContent() {
+  let sectionLeftContent = document.getElementsByClassName("left-content")[0];
+  main.removeChild(sectionLeftContent);
+}
+
+removeLeftContent();
+
+/* 🚀4 Centralize a section criado no passo 6 (aquele que possui a classe right-content). Dica: para centralizar, basta configurar o margin-right: auto da section; */
+
+function centerRightContent() {
+  let rightcontentToCenter = document.getElementsByClassName("right-content");
+  rightcontentToCenter[0].style.marginRight = "auto";
+}
+
+centerRightContent();
+
+/* 🚀5 Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content) para a cor verde; */
+
+function changeBackgroundToGreen() {
+  main.style.backgroundColor = "rgb(76,164,109)";
+}
+
+changeBackgroundToGreen();
+
+/* 🚀6 Remova os dois últimos elementos (nove e dez) da lista criada no passo 8. */
+
+function removeLast2Lis() {
+  let ulElement = document.getElementsByTagName("ul")[0];
+  ulElement.lastChild.remove();
+  ulElement.lastChild.remove();
+}
+removeLast2Lis();
